69301eb9d9e15f773e0532710006e5320b62959c
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-05-11 00:02:36 +0000

    modified:   blog/models.py 	modified:   blog/templates/blog/post_detail.html 	modified:   explorer/settings.py

--- explorer/settings.py
+++ explorer/settings.py
@@ -15,9 +15,11 @@ import os
 import sys
 from django.contrib.messages import constants as messages
 import dj_database_url
-if os.path.isfile('env.py'):
-    import env
-SECRET_KEY = os.environ.get("SECRET_KEY")    
+if os.path.isfile('env.py'):import env
+SECRET_KEY = os.environ.get("SECRET_KEY") 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
