e5760a67bbe9298cb1efe56c465f9665afe4fbb9
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-05-13 23:20:32 +0000

    coment delete url

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
    path('post_detial/<str:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
+    path('<slug:slug>/comment_delete/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
     path('edit_post/<int:post_id>',
          views.post_edit, name='post_edit'),
