2f8f3d7ea01dde13a275d4aff6f672e2f9e7269e
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-05-18 10:50:50 +0000

    fixed bug in comment delete or edit, in blog urls py

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('add_post', views.add_post, name='add_post'),
 
-   path('post_detial/<str:slug>/', views.post_detail, name='post_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
+    path('post_detial/<str:slug>/', views.post_detail, name='post_detail'),
+     path('post_detial/<str:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/comment_delete/<int:comment_id>',
+     path('post_detial/<str:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
     path('edit_post/<int:post_id>',
          views.post_edit, name='post_edit'),
