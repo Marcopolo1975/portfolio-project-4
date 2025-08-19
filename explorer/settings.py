e56c12025409a008e47e5c65737444445d2083d8
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2025-08-19 19:47:32 +0000

    remove code from settings py for elephent database

--- explorer/settings.py
+++ explorer/settings.py
@@ -16,17 +16,6 @@ from urllib.parse import urlparse, parse_qsl
 
 load_dotenv()
 
-from pathlib import Path
-import os
-import sys
-from django.contrib.messages import constants as messages
-import dj_database_url
-if os.path.isfile('env.py'):import env
-SECRET_KEY = os.environ.get("SECRET_KEY")
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
